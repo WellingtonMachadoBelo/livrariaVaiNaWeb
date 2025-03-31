980bf75e44a670cb8571a30155f2f94dd91f6159
WellingtonMachadoBelo <wellington.macbel@gmail.com>
2025-03-31 19:51:04 -0300

    atualização doados

--- src/pages/Doados/doados.jsx
+++ src/pages/Doados/doados.jsx
@@ -1,7 +1,23 @@
 import styles from "./doados.module.scss";
 import bookCover from "../../assets/bookCover.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Doados() {
+  const [livros, setLivros] = useState([]);
+
+  const getLivros = async () => {
+    const response = await axios.get(
+      "https://livros-doados.onrender.com/livros"
+    );
+
+    setLivros(response.data);
+  };
+
+  useEffect(() => {
+    getLivros();
+  }, []);
+
   return (
     <main className={styles.mainDoados}>
       <h2>Livros Doados</h2>
@@ -18,6 +34,14 @@ function Doados() {
           <p>Susanne Andrade</p>
           <p>Ficção</p>
         </article>
+        {livros.map((item) => (
+          <article className={styles.articleDoados} key={item.id}>
+            <img src={item.image - url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
       </section>
     </main>
   );
